9a23819797fdb1fe1e9f29b4f448f487312a15b1
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-05-28 23:38:16 +0300

    Добавьте файлы проекта.

---
+++ WpfYellowUA/Tech_Product_Model.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+namespace WpfYellowUA
+{
+    public class Tech_Product_Model : INotifyPropertyChanged
+    {
+
+        private string _name;
+        private int _price;
+
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { 
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+
+        public Tech_Product_Model(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = " ")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WpfYellowUA/Tech_Product_Model_Wiev.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+namespace WpfYellowUA
+{
+    class Tech_Product_Model_Wiev : INotifyPropertyChanged
+    {
+        public ObservableCollection<Tech_Product_Model> Tech_Products { get; set; }
+        public Tech_Product_Model_Wiev()
+        {
+            Tech_Products = new ObservableCollection<Tech_Product_Model>();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = " ")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
